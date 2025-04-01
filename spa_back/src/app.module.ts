047d8a68353ddd4e86314059a6267ca862143b37
nicov51 <nicolas.viennot@laposte.net>
2025-04-01 18:34:13 +0200

    create first module animal

--- spa_back/src/app.module.ts
+++ spa_back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AnimalModule } from './animal/animal.module';
 
 @Module({
-  imports: [],
+  imports: [AnimalModule],
   controllers: [AppController],
   providers: [AppService],
 })
